65b04dc1b2803fc1aad93ae5eefa5f0cad611188
Dhiraj Khapangi <dhiraj@rigonepal.com>
2023-06-22 16:50:34 +0545

    create single blog page

--- src/App.js
+++ src/App.js
@@ -3,18 +3,19 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Blog from "./pages/Blog";
+import SingleBlog from "./pages/SingleBlog";
 import About from "./pages/About";
 import Contact from "./pages/Contact";
 import OurStore from "./pages/OurStore";
 import Layout from "./components/Layout";
 import CompareProduct from "./pages/CompareProduct";
-
-import "./App.css";
 import Wishlist from "./pages/Wishlist";
 import Login from "./pages/Login";
 import ForgotPassword from "./pages/ForgotPassword";
 import Signup from "./pages/Signup";
 import Resetpassword from "./pages/Resetpassword";
+
+import "./App.css";
 
 function App() {
   return (
@@ -27,6 +28,7 @@ function App() {
             <Route path="contact" element={<Contact />} />
             <Route path="store" element={<OurStore />} />
             <Route path="blogs" element={<Blog />} />
+            <Route path="blog/:id" element={<SingleBlog />} />
             <Route path="compare-Product" element={<CompareProduct />} />
             <Route path="wishlist" element={<Wishlist />} />
             <Route path="login" element={<Login />} />
--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -14,7 +14,7 @@ const BlogCard = () => {
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cum,
           consequuntur.
         </p>
-        <Link to="/" className="button">
+        <Link to="/blog/:id" className="button">
           Read More
         </Link>
       </div>
